fcb252699acc97122414cff3a11b0bded261cd57
pollseed <cb.so.devope+0001@gmail.com>
2014-06-02 08:46:49 +0900

    first commit

--- BeaconNotificationApp/IBAViewController.m
+++ BeaconNotificationApp/IBAViewController.m
@@ -8,7 +8,16 @@
 
 #import "IBAViewController.h"
 
-@interface IBAViewController ()
+#import <CoreLocation/CoreLocation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface IBAViewController () <CLLocationManagerDelegate, CBPeripheralManagerDelegate>
+
+@property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) CBPeripheralManager *peripheralManager;
+@property (nonatomic) NSUUID *proximityUUID;
+@property (nonatomic) CLBeaconRegion *beaconRegion;
+@property (nonatomic) CLBeacon *nearestBeacon;
 
 @end
 
@@ -17,7 +26,28 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // 生成したUUID -> NSUUID
+    self.proximityUUID = [[NSUUID alloc] initWithUUIDString:@"79D2AA27-F2F3-477A-BA0E-A3AF88FB8CA1"];
+    
+    // CBPeripheralManager作成
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:nil];
+    
+    // アドバタイズ開始
+    if (self.peripheralManager.state == CBPeripheralManagerStatePoweredOn)
+    {
+        [self startAdvertising];
+    }
+}
+
+- (void)startAdvertising
+{
+    //CLBeaconRegionを作成->アドバタイズするデータを取得
+    CLBeaconRegion *beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:self.proximityUUID major:1 minor:2 identifier:@"jp.classmethod.testregion"];
+    NSDictionary *beaconPeripheralData = [beaconRegion peripheralDataWithMeasuredPower:nil];
+    
+    // アドバタイズを開始
+    [self.peripheralManager startAdvertising:beaconPeripheralData];
 }
 
 - (void)didReceiveMemoryWarning
